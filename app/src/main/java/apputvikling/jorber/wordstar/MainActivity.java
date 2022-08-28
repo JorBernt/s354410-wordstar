e62b1de6e17da46511d2fe8adf0298fcd438f27b
jorber <berntsen.jorgen@gmail.com>
2022-08-28 15:24:26 +0200

    Add language dropdown menu on main menu

--- app/src/main/java/apputvikling/jorber/wordstar/MainActivity.java
+++ app/src/main/java/apputvikling/jorber/wordstar/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,12 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main_menu);
+
+        Spinner languageMenu = findViewById(R.id.languageSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageMenu.setAdapter(adapter);
+
         Button playButton = findViewById(R.id.playButton);
         playButton.setText("Play");
         ProgressBar pb = findViewById(R.id.loadingBar);
         pb.setVisibility(View.INVISIBLE);
         playButton.setOnClickListener(view -> {
             playButton.setVisibility(View.INVISIBLE);
+            languageMenu.setVisibility(View.INVISIBLE);
             pb.setVisibility(View.VISIBLE);
             GameManager.instance().loadWordList(getAssets());
             Intent intent = new Intent(this, GameActivity.class);
