eae79d52a65b6ffd5aa8828013fe74d15210a5ee
jorber <berntsen.jorgen@gmail.com>
2022-08-28 12:24:31 +0200

    Move game logic to its own manager class

---
+++ app/src/main/java/apputvikling/jorber/wordstar/GameManager.java
@@ -0,0 +1,144 @@
+package apputvikling.jorber.wordstar;
+
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GameManager {
+
+    private static GameManager instance;
+    private final List<String> words = new ArrayList<>();
+    private Set<String> matchingWords;
+    private List<Character> chosenCharacters;
+    private char chosenChar;
+    private static int points = 0;
+
+    private GameManager() { }
+
+    public static GameManager instance() {
+        return instance = instance == null ? new GameManager() : instance;
+    }
+
+    public void initalizeGame() {
+        while (true) {
+            chosenCharacters = getCharacters();
+            chosenChar = chosenCharacters.get(new Random().nextInt(7));
+            matchingWords = getWords(chosenCharacters, chosenChar);
+            chosenCharacters.remove(Character.valueOf(chosenChar));
+            if (matchingWords.size() >= 15)
+                break;
+        }
+        System.out.println(chosenChar);
+        for (String s : matchingWords)
+            System.out.println(s);
+    }
+
+    private boolean containsCharacters(String s, List<Character> characters) {
+        for (char c : s.toCharArray())
+            if (!characters.contains(c))
+                return false;
+        return true;
+    }
+
+    public void loadWordList(AssetManager am) {
+        try {
+            Scanner in = new Scanner(new InputStreamReader(am.open("no-wordlist.txt"), StandardCharsets.UTF_8));
+            while (in.hasNext()) {
+                words.add(in.nextLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void shuffleCharactersButtons(Map<Integer, MainActivity.CharButton> buttonMap) {
+        Collections.shuffle(chosenCharacters);
+        int index = 0;
+        for (MainActivity.CharButtons button : MainActivity.CharButtons.values()) {
+            if (button == MainActivity.CharButtons.MID_MID)
+                continue;
+            MainActivity.CharButton b = buttonMap.get(button.id);
+            b.setLetter(chosenCharacters.get(index++));
+            b.getButton().setText(Character.toString(b.getLetter()));
+        }
+    }
+
+    public int submitAnswer(String input) {
+        if (validWord(input)) {
+            int p = getWordPointValue(input);
+            points += p;
+            return p;
+        }
+        return 0;
+    }
+
+    private boolean validWord(String word) {
+        for (String s : words)
+            if (word.equals(s))
+                return true;
+        return false;
+    }
+
+    private Set<String> getWords(List<Character> characters, Character chosenCharacter) {
+        Set<String> matchingWords = new HashSet<>();
+        for (String s : words) {
+            if (containsCharacters(s, characters) && s.contains(chosenCharacter + ""))
+                matchingWords.add(s);
+        }
+        return matchingWords;
+    }
+
+    private List<Character> getCharacters() {
+        List<String> characters = Arrays.stream("abcdefghijklmnopqrstuvwxyzæøå".toUpperCase().split("")).collect(Collectors.toList());
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < 30; i++)
+            numbers.add(i);
+        Collections.shuffle(numbers);
+        List<Character> chosen = new ArrayList<>();
+        for (int i = 0; i < 7; i++)
+            chosen.add(characters.get(numbers.get(i)).charAt(0));
+        return chosen;
+    }
+
+    public List<Character> getChosenCharacters() {
+        return chosenCharacters;
+    }
+
+    public Character getChosenCharacter() {
+        return chosenChar;
+    }
+
+    public String getHint() {
+        String hint = "";
+        int i = 0;
+        int randomIndex = new Random().nextInt(matchingWords.size());
+        for (String s : matchingWords) {
+            if (i++ == randomIndex) {
+                hint = s;
+                break;
+            }
+        }
+        int mid = hint.length() / 2;
+        return String.format("Hint: %s**%s", hint.substring(0, mid - 1), hint.substring(mid + 1));
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    private int getWordPointValue(String word) {
+        return 10;
+    }
+}
--- app/src/main/java/apputvikling/jorber/wordstar/MainActivity.java
+++ app/src/main/java/apputvikling/jorber/wordstar/MainActivity.java
@@ -6,11 +6,9 @@ import android.os.Bundle;
 import android.text.Html;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -37,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
             this.id = id;
             this.button = button;
         }
+
+        public int getId() {
+            return id;
+        }
+
+        public char getLetter() {
+            return letter;
+        }
+
+        public void setLetter(Character c) {
+            this.letter = c;
+        }
+
+        public Button getButton() {
+            return button;
+        }
+
+        public boolean isChosen() {
+            return chosen;
+        }
     }
 
     enum CharButtons {
@@ -55,75 +73,92 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private final List<String> words = new ArrayList<>();
-    private Set<String> matchingWords;
-    List<Character> chosenCharacters;
-    private final Map<Integer, CharButton> charButtonsMap = new HashMap<>();
-    private char chosenChar;
-    private static int points = 0;
 
-    TextView textInput, pointsView, hintView;
+    private final Map<Integer, MainActivity.CharButton> charButtonsMap = new HashMap<>();
+
+    TextView textInput, pointsView, hintView, popupTextView;
+    RelativeLayout popUpView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GameManager.instance().loadWordList(getAssets());
         getSupportActionBar().hide();
+
         for (CharButtons button : CharButtons.values()) {
             Button b = findViewById(button.id);
-            b.setOnClickListener(view -> handleCharacterButton(view.getId()));
+            b.setOnClickListener(view -> {
+                if (textInput.getText().toString().length() >= 18)
+                    return;
+                CharButton cbutton = charButtonsMap.get(view.getId());
+                if (cbutton.chosen) {
+                    textInput.append(Html.fromHtml("<font color=#ff0000>" + cbutton.letter + "</font>", Html.FROM_HTML_MODE_LEGACY));
+                } else {
+                    textInput.append(Character.toString(cbutton.letter));
+                }
+            });
             charButtonsMap.put(button.id, new CharButton(button.id, b));
         }
 
         Button deleteBtn = findViewById(R.id.deleteBtn);
         deleteBtn.setText(R.string.delete_btn);
 
+        deleteBtn.setOnClickListener(view -> {
+            if (textInput.length() < 1)
+                return;
+            String text = textInput.getText().toString();
+            text = text.substring(0, text.length() - 1);
+            textInput.setText("");
+            for (char c : text.toCharArray()) {
+                if (c == GameManager.instance().getChosenCharacter()) {
+                    textInput.append(Html.fromHtml("<font color=#ff0000>" + c + "</font>", Html.FROM_HTML_MODE_LEGACY));
+                    continue;
+                }
+                textInput.append(Character.toString(c));
+            }
+        });
+
         Button shuffleBtn = findViewById(R.id.shuffleBtn);
         shuffleBtn.setText(R.string.shuffle_btn);
-        shuffleBtn.setOnClickListener(view -> handleShuffleButton());
+
+        shuffleBtn.setOnClickListener(view -> GameManager.instance().shuffleCharactersButtons(charButtonsMap));
 
         Button submitBtn = findViewById(R.id.submitBtn);
         submitBtn.setText(R.string.submit_btn);
-        submitBtn.setOnClickListener(view -> handleSubmitButton());
+
+        submitBtn.setOnClickListener(view -> {
+            String input = textInput.getText().toString();
+            int points = GameManager.instance().submitAnswer(input);
+            if (points > 0) {
+                showMessage(String.format("Correct word! Points: %s", points));
+                updatePoints(points);
+            } else {
+                showMessage("Invalid word");
+            }
+            reset();
+        });
 
         Button hintButton = findViewById(R.id.hintBtn);
         hintButton.setText(R.string.hint_btn);
-        hintButton.setOnClickListener(view -> handleHintButton());
+        hintButton.setOnClickListener(view -> hintView.setText(GameManager.instance().getHint()));
 
         textInput = findViewById(R.id.textInput);
         textInput.setTextSize(36);
         pointsView = findViewById(R.id.pointView);
-        pointsView.setText(String.format("Poeng: %s", points));
+        pointsView.setText(String.format("Poeng: %s", GameManager.instance().getPoints()));
         hintView = findViewById(R.id.hintView);
 
         ProgressBar pb = findViewById(R.id.scoreProgressBar);
 
-        findViewById(R.id.deleteBtn).setOnClickListener(view -> handleDeleteButton());
-        try {
-            Scanner in = new Scanner(new InputStreamReader(getAssets().open("no-wordlist.txt"), StandardCharsets.UTF_8));
-            while (in.hasNext()) {
-                words.add(in.nextLine());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        initalizeGame();
+        initializeGame();
     }
 
-    private void initalizeGame() {
-        while (true) {
-            chosenCharacters = getCharacters();
-            chosenChar = chosenCharacters.get(new Random().nextInt(7));
-            matchingWords = getWords(chosenCharacters, chosenChar);
-            chosenCharacters.remove(Character.valueOf(chosenChar));
-            if (matchingWords.size() >= 15)
-                break;
-        }
-        System.out.println(chosenChar);
-        for (String s : matchingWords)
-            System.out.println(s);
+    private void initializeGame() {
+        GameManager.instance().initalizeGame();
         int charIndex = 0;
         CharButton midButton = charButtonsMap.get(CharButtons.MID_MID.id);
+        Character chosenChar = GameManager.instance().getChosenCharacter();
         midButton.letter = chosenChar;
         midButton.chosen = true;
         midButton.button.setText(Character.toString(chosenChar));
@@ -131,54 +166,15 @@ public class MainActivity extends AppCompatActivity {
             if (button.equals(CharButtons.MID_MID))
                 continue;
             CharButton b = charButtonsMap.get(button.id);
-            b.letter = chosenCharacters.get(charIndex);
-            b.button.setText(Character.toString(chosenCharacters.get(charIndex++)));
+            b.letter = GameManager.instance().getChosenCharacters().get(charIndex);
+            b.button.setText(Character.toString(GameManager.instance().getChosenCharacters().get(charIndex++)));
         }
     }
 
-    private Set<String> getWords(List<Character> characters, Character chosenCharacter) {
-        Set<String> matchingWords = new HashSet<>();
-        for (String s : words) {
-            if (containsCharacters(s, characters) && s.contains(chosenCharacter + ""))
-                matchingWords.add(s);
-        }
-        return matchingWords;
-    }
 
-
-    private boolean containsCharacters(String s, List<Character> characters) {
-        for (char c : s.toCharArray())
-            if (!characters.contains(c))
-                return false;
-        return true;
-    }
-
-    private boolean validWord(String word) {
-        for (String s : words)
-            if (word.equals(s))
-                return true;
-        return false;
-    }
-
-    private int getWordPointValue(String word) {
-        return 10;
-    }
-
-    private List<Character> getCharacters() {
-        List<String> characters = Arrays.stream("abcdefghijklmnopqrstuvwxyzæøå".toUpperCase().split("")).collect(Collectors.toList());
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i < 30; i++)
-            numbers.add(i);
-        Collections.shuffle(numbers);
-        List<Character> chosen = new ArrayList<>();
-        for (int i = 0; i < 7; i++)
-            chosen.add(characters.get(numbers.get(i)).charAt(0));
-        return chosen;
-    }
 
     private void updatePoints(int n) {
-        points += n;
-        pointsView.setText(String.format("Poeng: %d", points));
+        pointsView.setText(String.format("Poeng: %s", n));
     }
 
     private void showMessage(String s) {
@@ -189,69 +185,5 @@ public class MainActivity extends AppCompatActivity {
         textInput.setText("");
     }
 
-    private void handleShuffleButton() {
-        Collections.shuffle(chosenCharacters);
-        int index = 0;
-        for(CharButtons button : CharButtons.values()) {
-            if(button == CharButtons.MID_MID)
-                continue;
-            CharButton b = charButtonsMap.get(button.id);
-            b.letter = chosenCharacters.get(index++);
-            b.button.setText(Character.toString(b.letter));
 
-        }
-    }
-
-    private void handleHintButton() {
-        String hint = "";
-        int i = 0;
-        int randomIndex = new Random().nextInt(matchingWords.size());
-        for (String s : matchingWords) {
-            if (i++ == randomIndex) {
-                hint = s;
-                break;
-            }
-        }
-        int mid = hint.length() / 2;
-        hintView.setText(String.format("Hint: %s**%s", hint.substring(0, mid - 1), hint.substring(mid + 1)));
-    }
-
-    private void handleDeleteButton() {
-        if (textInput.length() < 1)
-            return;
-        String text = textInput.getText().toString();
-        text = text.substring(0, text.length() - 1);
-        textInput.setText("");
-        for (char c : text.toCharArray()) {
-            if (c == chosenChar) {
-                textInput.append(Html.fromHtml("<font color=#ff0000>" + c + "</font>", Html.FROM_HTML_MODE_LEGACY));
-                continue;
-            }
-            textInput.append(Character.toString(c));
-        }
-
-    }
-
-    private void handleCharacterButton(int id) {
-        if (textInput.getText().toString().length() >= 18)
-            return;
-        CharButton button = charButtonsMap.get(id);
-        if (button.chosen) {
-            textInput.append(Html.fromHtml("<font color=#ff0000>" + button.letter + "</font>", Html.FROM_HTML_MODE_LEGACY));
-        } else {
-            textInput.append(Character.toString(button.letter));
-        }
-    }
-
-    private void handleSubmitButton() {
-        String input = textInput.getText().toString();
-        if (validWord(input)) {
-            updatePoints(getWordPointValue(input));
-            System.out.println("riktig!");
-        } else {
-            showMessage("Invalid word");
-            System.out.println("feil");
-        }
-        reset();
-    }
 }
